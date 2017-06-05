5fb78161efb76e0592037672e5d20482aca0c7c3
W. Lee Pang <wleepang@gmail.com>
2017-06-04 22:05:09 -0700

    refactor for concurrent execution

--- version_support.py
+++ version_support.py
@@ -23,9 +23,11 @@ Filter the list for packages with the following metadata properties:
     - Development Status >= 4 and <= 6 (may be too stringent)
     - Programming Language :: Python :: <version>
 """
+import re
 import logging
+import concurrent.futures as cf
+from collections import defaultdict
 
-from tqdm import tqdm
 import requests
 from bs4 import BeautifulSoup
 
@@ -36,6 +38,58 @@ logging.basicConfig(level=logging.INFO)
 class SkippingError(Exception):
     pass
 
+
+def get_versions(classifiers):
+    versions = list(filter(
+        lambda x: re.search('Python :: \d\.\d', x),
+        classifiers
+    ))
+
+    if versions:
+        versions = list(map(
+            lambda x: float(re.split('\s::\s', x)[2]),
+            versions
+        ))
+
+    return versions
+
+
+def read_package(pkg):
+    resp = requests.get(f'https://pypi.python.org/pypi/{pkg}/json')
+    skip = f'skip {pkg}'
+    keep = f'keep {pkg}'
+
+    versions = []
+    try:
+        if not resp.status_code == 200:
+            raise SkippingError('error retrieving metadata')
+
+        props = resp.json()
+        info = props.get('info')
+
+        if not info:
+            raise SkippingError('no info')
+        
+        # summary = info.get('summary')
+        # if not summary or summary == 'UNKOWN':
+        #     raise SkippingError('no summary')
+        
+        classifiers = info.get('classifiers')
+        if not classifiers or not len(classifiers):
+            raise SkippingError('no classifiers')
+
+        logging.info(f'{keep}')
+        versions = get_versions(classifiers)
+
+    except SkippingError as e:
+        reason = str(e)
+        logging.info(f'{skip}: {reason}')
+
+    return versions
+    
+
+PKG_INFO = {}
+VERSIONS = defaultdict(lambda: 0)
 
 if __name__ == '__main__':
 
@@ -50,33 +104,20 @@ if __name__ == '__main__':
     pkgs = [link.get('href') for link in soup.find_all('a')]
     logging.info('{} packages found'.format(len(pkgs)))
 
-    progress = tqdm(pkgs)
-    for pkg in progress:
-        resp = requests.get(f'https://pypi.python.org/pypi/{pkg}/json')
-        skip = f'skipping {pkg}'
-        keep = f'keeping {pkg}'
+    # for pkg in pkgs:
+    #     versions = read_package(pkg)
 
-        try:
-            if not resp.status_code == 200:
-                raise SkippingError('error retrieving metadata')
+    with cf.ThreadPoolExecutor() as executor:
+        futures = {executor.submit(read_package, pkg): pkg for pkg in pkgs}
+        for future in cf.as_completed(futures):
+            pkg = futures[future]
+            versions = future.result()
 
-            props = resp.json()
-            info = props.get('info')
+            PKG_INFO[pkg] = versions
 
-            if not info:
-                raise SkippingError('no info')
-            
-            if not info.get('summary') or info.get('summary') == 'UNKOWN':
-                raise SkippingError('no summary')
-
-            if not info.get('classifiers') or not len(info.get('classifiers')):
-                raise SkippingError('no classifiers')
-
-        except SkippingError as e:
-            reason = str(e)
-            progress.set_description(f'{skip}: {reason}')
-            continue
-        
-        progress.set_description(f'{keep}')
-
+            if len(versions):
+                for version in versions:
+                    VERSIONS[version] += 1
+            else:
+                VERSIONS['unknown'] += 1
 
