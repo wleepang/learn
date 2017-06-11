24982211ae9b46fcb072d82bd40fd5c2ed9213b4
W. Lee Pang <wleepang@gmail.com>
2017-06-10 21:57:18 -0700

    update main processing
    * set / optimize max worker pool size based in cpu count
    * add reporting of elapsed timing
    * send results to file(s)

--- version_support.py
+++ version_support.py
@@ -12,7 +12,7 @@ be used when users install packages - i.e. via `pip`
 Lists published packages via PyPI's `simple` API:
 https://pypi.python.org/simple/
 
-Iterate through the list and retrieve package metadata using the 
+Iterate through the list and retrieve package metadata using the
 JSON API:
 https://pypi.python.org/pypi/<package>/json
 
@@ -26,40 +26,49 @@ Filter the list for packages with the following metadata properties:
 import re
 import logging
 import concurrent.futures as cf
-from collections import defaultdict
+import multiprocessing as mp
+import json
+from collections import defaultdict, namedtuple
+from pprint import pprint
+from datetime import datetime
 
 import requests
 from bs4 import BeautifulSoup
 
 
 logging.basicConfig(level=logging.INFO)
-
+MAX_PROCESSES = mp.cpu_count()*8
 
 class SkippingError(Exception):
     pass
 
 
+PkgInfo = namedtuple('PkgInfo', ['versions', 'error'])
+
 def get_versions(classifiers):
-    versions = list(filter(
-        lambda x: re.search('Python :: \d\.\d', x),
-        classifiers
-    ))
+    versions = [
+        c for c in classifiers if re.search('Python :: \d(\.\d)?', c)
+    ]
 
     if versions:
-        versions = list(map(
-            lambda x: float(re.split('\s::\s', x)[2]),
-            versions
-        ))
+        versions = [
+            re.split('\s::\s', v)[2].strip()
+            for v in versions
+        ]
 
-    return versions
+    return list(set(versions))
 
 
-def read_package(pkg):
+def read_package(pkg, id_=None):
     resp = requests.get(f'https://pypi.python.org/pypi/{pkg}/json')
-    skip = f'skip {pkg}'
-    keep = f'keep {pkg}'
+    if id_:
+        id_ = f'[{id_:7d}] '
+
+    skip = f'{id_}skip {pkg}'
+    keep = f'{id_}keep {pkg}'
 
     versions = []
+    error = None
     try:
         if not resp.status_code == 200:
             raise SkippingError('error retrieving metadata')
@@ -69,11 +78,7 @@ def read_package(pkg):
 
         if not info:
             raise SkippingError('no info')
-        
-        # summary = info.get('summary')
-        # if not summary or summary == 'UNKOWN':
-        #     raise SkippingError('no summary')
-        
+
         classifiers = info.get('classifiers')
         if not classifiers or not len(classifiers):
             raise SkippingError('no classifiers')
@@ -82,17 +87,20 @@ def read_package(pkg):
         versions = get_versions(classifiers)
 
     except SkippingError as e:
-        reason = str(e)
-        logging.info(f'{skip}: {reason}')
+        error = str(e)
+        logging.info(f'{skip}: {error}')
 
-    return versions
-    
+    return PkgInfo(versions, error)
+
 
 PKG_INFO = {}
 VERSIONS = defaultdict(lambda: 0)
 
 if __name__ == '__main__':
 
+    time_start = datetime.now()
+
+    logging.info('started: {}'.format(time_start.strftime('%F %X')))
     logging.info('retrieving packages list')
     resp = requests.get('https://pypi.python.org/simple')
     if not resp.status_code == 200:
@@ -104,20 +112,35 @@ if __name__ == '__main__':
     pkgs = [link.get('href') for link in soup.find_all('a')]
     logging.info('{} packages found'.format(len(pkgs)))
 
-    # for pkg in pkgs:
-    #     versions = read_package(pkg)
-
-    with cf.ThreadPoolExecutor() as executor:
-        futures = {executor.submit(read_package, pkg): pkg for pkg in pkgs}
+    time_parse_start = datetime.now()
+    with cf.ThreadPoolExecutor(MAX_PROCESSES) as executor:
+        futures = {executor.submit(read_package, pkg, id_): pkg for id_, pkg in enumerate(pkgs)}
         for future in cf.as_completed(futures):
             pkg = futures[future]
-            versions = future.result()
+            info = future.result()
 
-            PKG_INFO[pkg] = versions
-
-            if len(versions):
-                for version in versions:
+            PKG_INFO[pkg] = info
+            VERSIONS['total'] += 1
+            if len(info.versions):
+                VERSIONS['known'] += 1
+                for version in info.versions:
                     VERSIONS[version] += 1
             else:
-                VERSIONS['unknown'] += 1
+                if not info.error:
+                    VERSIONS['nd_unk'] += 1
+                else:
+                    VERSIONS['na'] += 1
 
+    time_end = datetime.now()
+    time_parse_elapsed = time_end - time_parse_start
+    time_elapsed = time_end - time_start
+
+    with open('PKG_INFO.json', 'w') as pkgfile, open('VERSIONS.json', 'w') as verfile:
+        json.dump(PKG_INFO, pkgfile)
+        json.dump(VERSIONS, verfile)
+
+    logging.info('finished: {}'.format(time_end.strftime('%F %X')))
+    logging.info('parsing elapsed: {}'.format(str(time_parse_elapsed)))
+    logging.info('total elapsed: {}'.format(str(time_elapsed)))
+
+    pprint(VERSIONS)
